e63495ec173a0c5d939eaf8466b419d58edeb264
Cyber Naskar <cybr1998@gmail.com>
2016-07-07 20:34:57 +0530

    Create Calculator.h

---
+++ Calculator_Project/Calculator.h
@@ -0,0 +1,25 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+
+class Calculator
+{
+    public:
+        Calculator();
+        void printCalc();
+        void GetExp();
+        void Add();
+        void Subtract();
+        void Multiply();
+        void Divide();
+        void UpdateResult();
+        void ShowResult();
+        void ShowMenu();
+
+
+    protected:
+
+    private:
+};
+
+#endif // CALCULATOR_H
